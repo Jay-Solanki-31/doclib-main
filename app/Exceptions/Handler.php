5f7902a1430c736ad6118b481b07a8aa4f28a2ff
Jay Solanki <jaysolanki1733@gmail.com>
2024-08-20 11:56:54 +0530

    first commit

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Exceptions;
+
+use Throwable;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Throwable  $e
+     * @return void
+     */
+    public function report(Throwable $e)
+    {
+        parent::report($e);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $e
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $e)
+    {
+        return parent::render($request, $e);
+    }
+
+    /**
+     * Handle unauthenticated user access.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+        if ($request->is('librarian') || $request->is('librarian/*')) {
+            return redirect()->guest('/login/librarian');
+        }
+        if ($request->is('teacher') || $request->is('teacher/*')) {
+            return redirect()->guest('/login/teacher');
+        }
+        if ($request->is('student') || $request->is('student/*')) {
+            return redirect()->guest('/login/student');
+        }
+        return redirect()->guest(route('login'));
+    }
+}
